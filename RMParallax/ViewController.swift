0360a266821b5d5c0d4d55bdcd60ac7dfabfb496
Ben <benaneesh@gmail.com>
2016-02-21 11:00:20 -0800

    added delegate

--- RMParallax/ViewController.swift
+++ RMParallax/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
         let item2 = RMParallaxItem(image: UIImage(named: "item2")!, text: "FOLLOW WORLD CLASS PHOTOGRAPHERS")
         let item3 = RMParallaxItem(image: UIImage(named: "item3")!, text: "EXPLORE OUR COLLECTION BY CATEGORY")
         
-        let rmParallaxViewController = RMParallax(items: [item1, item2, item3], motion: false) 
+        let rmParallaxViewController = RMParallax(items: [item1, item2, item3], motion: false)
+        rmParallaxViewController.delegate = self
         rmParallaxViewController.completionHandler = {
             UIView.animateWithDuration(0.4, animations: { () -> Void in
                 rmParallaxViewController.view.alpha = 0.0
